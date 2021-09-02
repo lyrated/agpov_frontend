6ee1fc73372b0fd1a57c3459fe26da17e78651f8
Ly Nguyen <mly.nguyen@yahoo.de>
2021-09-03 00:36:18 +0200

    footer adjusted according to tmdb TOS

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <p>
-        Copyright &#169; 2021 M. L. Nguyen. <em>This web app is part of my bachelor thesis at HTW Berlin. The data was provided by <a href="https://tmdb.org/" target="_blank" rel="noopener noreferrer">TMDb.org</a> (data retrieved on April 12th, 2021).</em>
+        Copyright &#169; 2021 M. L. Nguyen. <em>This web app is part of my bachelor thesis at HTW Berlin. This product uses the TMDB API but is not endorsed or certified by TMDB. (data retrieved on April 12th, 2021)</em>
       </p>
     </footer>
   );
